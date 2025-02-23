b1f5a4f6ca80a1c3b11d2eb89e4620880fdc9338
saurabh sharma <saurabhcoded@gmail.com>
2025-02-24 00:34:08 +0530

    Updated Path to I18n

--- api/src/config/i18n.config.js
+++ api/src/config/i18n.config.js
@@ -1,11 +1,11 @@
-const i18n = require('i18n');
-const path = require('path');
+// const i18n = require('i18n');
+// const path = require('path');
 
-// internalisation
-i18n.configure({
-    locales: ['en', 'hi'],
-    defaultLocale: 'en',
-    directory: path.join('src', 'locales'),
-})
+// // internalisation
+// i18n.configure({
+//     locales: ['en', 'hi'],
+//     defaultLocale: 'en',
+//     directory: path.join('src', 'locales'),
+// })
 
-module.exports = i18n;
+// module.exports = i18n;
